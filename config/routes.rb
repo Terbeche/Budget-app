5f4448736fd1b105b28d6f28f91e1f03ab0de9b6
terbeche <termus96@gmail.com>
2022-10-10 14:28:56 +0100

    Setup authentication for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resources :users do
+    resources :groups
+    resources :operations
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "users#index"
 end
