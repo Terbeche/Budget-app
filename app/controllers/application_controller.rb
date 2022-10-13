98aad797d37cf3598ee66da080e4946b33ee09ac
terbeche <termus96@gmail.com>
2022-10-13 19:21:57 +0100

    Add splash screen for the app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def update_allowed_parameters
@@ -14,6 +13,10 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name])
+  end
+
+  def after_sign_in_path_for(_scope)
+    home_path
   end
 
   def after_sign_out_path_for(_scope)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'users/groups'
   get 'users/operations'
+  get '/splash', to: 'splash#index', as: 'splash'
+  get '/home', to: 'home#index', as: 'home'
+
   devise_for :users
   resources :users, only: [ :index, :show] do
     resources :groups, only: [ :index, :show, :new, :create] do
@@ -12,5 +15,5 @@ Rails.application.routes.draw do
     root :to => "home#index"
   end
   
-  root to: "home#index"
+  root to: "splash#index"
 end
